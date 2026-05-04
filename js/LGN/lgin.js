0553cd9e0ba2d0dcabc746104f071dceafd678dd
meru-sys <kakoro323@gmail.com>
2026-05-05 00:32:08 +0900

    main画面の作成、loadingの作成、画面遷移時のフラッシュを修正

--- js/LGN/lgin.js
+++ js/LGN/lgin.js
@@ -1,3 +1,22 @@
 function goHome() {
-    window.location.href = "main/home.html";
+    // ① フェードアウト開始
+    document.body.classList.add("fade-out");
+
+    setTimeout(() => {
+        const overlay = document.createElement("div");
+        overlay.classList.add("loading-screen");
+
+        overlay.innerHTML = `
+            <div class="spinner"></div>
+            <p>Loading...</p>
+        `;
+
+        // ここが重要（bodyじゃなくhtmlに追加）
+        document.documentElement.appendChild(overlay);
+
+        setTimeout(() => {
+            window.location.href = "main/home.html";
+        }, 800);
+
+    }, 500);
 }
